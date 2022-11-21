a91e487fec1c4a8bb7e901670d510ec76f29d048
thanhvdt <tthanhvuong11@gmail.com>
2022-11-21 14:22:20 +0700

    Clean code

--- src/components/EnPage/DictLang.js
+++ src/components/EnPage/DictLang.js
@@ -7,25 +7,20 @@ import React from "react";
 function DictLang({ fromEng, handleClick }) {
   return (
     <div className="header__dictlang">
-      {fromEng ? (
-        <>
-          <div className="header__dictlang-item" id="header__dictlang-en">
-            <h6 className="header__dictlang-item-title">Tiếng Anh</h6>
-          </div>
-          <div className="header__dictlang-item" id="header__dictlang-vn">
-            <h6 className="header__dictlang-item-title">Tiếng Việt</h6>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="header__dictlang-item" id="header__dictlang-vn">
-            <h6 className="header__dictlang-item-title">Tiếng Việt</h6>
-          </div>
-          <div className="header__dictlang-item" id="header__dictlang-en">
-            <h6 className="header__dictlang-item-title">Tiếng Anh</h6>
-          </div>
-        </>
-      )}
+      <div className="header__dictlang-item" id="header__dictlang-en">
+        {fromEng ? (
+          <h6 className="header__dictlang-item-title">English</h6>
+        ) : (
+          <h6 className="header__dictlang-item-title">Vietnamese</h6>
+        )}
+      </div>
+      <div className="header__dictlang-item" id="header__dictlang-vn">
+        {fromEng ? (
+          <h6 className="header__dictlang-item-title">Vietnamese</h6>
+        ) : (
+          <h6 className="header__dictlang-item-title">English</h6>
+        )}
+      </div>
 
       <button
         className="header__dictlang-btn"
